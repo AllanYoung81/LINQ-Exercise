f0922cc4537e05fa41b62251e8e42e33eb2d2547
Allan Young <ayounggtr@gmail.com>
2023-11-22 11:43:45 -0500

    LINQ Exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> retroVideoGames = new List<string>() { "Pac Man", "Donkey Kong", "Ms Pac Man", "Galaga" };
+
+            retroVideoGames.Add("Centipede");
+            retroVideoGames.Add("Defender");
+
+            var videoGames = retroVideoGames.OrderBy(name => name.Length); //OrderByDescend to go in reverse
+
+            foreach (var game in videoGames) 
+            
+            { 
+                Console.WriteLine(game);
+            
+            }
+
         }
     }
 }
